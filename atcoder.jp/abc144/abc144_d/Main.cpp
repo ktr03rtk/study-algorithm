2d00f6185e655e7b4ac3838eb8712dfb80ac51dc
ktr03 <52913281+ktr03rtk@users.noreply.github.com>
2022-09-16 23:31:43 +0900

    [AC] abc144 abc144_d

---
+++ atcoder.jp/abc144/abc144_d/Main.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+#define rep(i, j, n) for (int i = j; i < (int)(n); i++)
+
+using namespace std;
+using ll = long long;
+
+int main() {
+  double a, b, x;
+  cin >> a >> b >> x;
+
+  double volume = a * a * b;
+  double ans = 0;
+  if (x < volume / 2) {
+    ans = atan(a * b * b / (2.0 * x));
+  } else {
+    ans = atan((2.0 * b - 2.0 * x / (a * a)) / a);
+  }
+
+  cout << fixed << setprecision(10) << ans / M_PI * 180 << endl;
+}
