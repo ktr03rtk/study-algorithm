6516bf045d45da6db1dc7c2755fd8f74fa96b39a
ktr03 <52913281+ktr03rtk@users.noreply.github.com>
2022-09-24 21:06:35 +0900

    [AC] abc270 abc270_a

---
+++ atcoder.jp/abc270/abc270_a/Main.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+#define rep(i, j, n) for (int i = j; i < (int)(n); i++)
+
+using namespace std;
+using ll = long long;
+
+int main() {
+  int A, B;
+  cin >> A >> B;
+
+  int ans = 0;
+  ans = A | B;
+
+  cout << ans << endl;
+}
