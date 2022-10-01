53a6107b1ecffee95c5a66fb05753d45b21a4337
ktr03 <52913281+ktr03rtk@users.noreply.github.com>
2022-10-01 21:11:15 +0900

    [AC] abc271 abc271_a

---
+++ atcoder.jp/abc271/abc271_a/Main.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+
+#define rep(i, j, n) for (int i = j; i < (int)(n); i++)
+
+using namespace std;
+using ll = long long;
+
+int main() {
+  int N;
+  cin >> N;
+  printf("%02X\n", N);
+}
