7d3222973f29c4bac79dec8a56aba0fa69bcb0d4
ktr03 <52913281+ktr03rtk@users.noreply.github.com>
2022-10-15 22:32:08 +0900

    [AC] abc273 abc273_c

---
+++ atcoder.jp/abc273/abc273_c/Main.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+
+#define rep(i, j, n) for (int i = j; i < (int)(n); i++)
+
+using namespace std;
+using ll = long long;
+
+int main() {
+  int N;
+  cin >> N;
+  vector<int> A(N);
+  vector<int> B(N);
+  rep(i, 0, N) {
+    int a;
+    cin >> a;
+    A[i] = a;
+    B[i] = a;
+  }
+  sort(B.begin(), B.end());
+  B.erase(unique(B.begin(), B.end()), B.end());
+
+  vector<int> C(N, 0);
+  rep(i, 0, N) {
+    auto itr = upper_bound(B.begin(), B.end(), A[i]);
+    if (itr != B.end()) {
+      int dist = distance(itr, B.end());
+      C[dist]++;
+    } else {
+      C[0]++;
+    }
+  }
+
+  rep(i, 0, N) { cout << C[i] << endl; }
+}
